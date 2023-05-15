d5bbe9be5a031882066957a32d1a05a3c0a6e664
Brad Garropy <bgarropy@atlassian.com>
2023-05-15 14:23:37 -0500

    static output.

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,6 @@ export default defineConfig({
     markdown: {
         remarkPlugins: [remarkReadingTime],
     },
-    output: "server",
+    output: "static",
     site: "https://rhythm.nation",
 })
