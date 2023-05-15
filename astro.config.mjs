3d7c460f12ba0509a996bfc256d879c20ff39dcb
Brad Garropy <bgarropy@atlassian.com>
2023-05-15 14:29:03 -0500

    serverless adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import {defineConfig} from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import image from "@astrojs/image"
-import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel/static"
 import sitemap from "@astrojs/sitemap"
 import {remarkReadingTime} from "./src/remarkReadingTime"
 
